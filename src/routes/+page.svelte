7a5b0412ccfa6c718fa213acfa88ac6f894e5ee2
SamBat2005 <batcsg1@student.op.ac.nz>
2025-01-06 20:29:06 +1300

    Added js for rendering data into main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    export let data;
+    //Test to see if data comes through
+    console.log(data.planets);
+
+</script>
+
+<style>
+
+</style>
